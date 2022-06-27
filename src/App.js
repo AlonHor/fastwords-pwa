acb6904067797a0a79e4664dfc84728a7c934241
Alon <alon.horesh.08@gmail.com>
2022-06-27 23:58:28 +0300

    Created a PWA

--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import Word from './components/Word'
 
 function App() {
+  const [words, setWords] = useState([])
+  const [erroredWord, setErroredWord] = useState([])
+  const [originalWord, setOriginalWord] = useState([])
+  const [disabled, setDisabled] = useState(false)
+  const [initialTime, setInitialTime] = useState(new Date())
+  const [time, setTime] = useState(0)
+
+  let allWords = []
+
+  function createErroredWord(word, consonants, vowels) {
+    const ORIGINAL_WORD = word
+    let i = 0
+    while (i < 10) {
+      let candidateForReplacement1 = Math.floor(Math.random() * word.length)
+      let candidateForReplacement2 = Math.floor(Math.random() * word.length)
+      if (candidateForReplacement1 === candidateForReplacement2) {
+        i++
+        continue
+      }
+      if (
+        vowels.has(word[candidateForReplacement1]) &&
+        consonants.has(word[candidateForReplacement2])
+      ) {
+        i++
+        continue
+      }
+      if (
+        vowels.has(word[candidateForReplacement2]) &&
+        consonants.has(word[candidateForReplacement1])
+      ) {
+        i++
+        continue
+      }
+      let temp = word[candidateForReplacement1]
+      word =
+        word.substring(0, candidateForReplacement1) +
+        word[candidateForReplacement2] +
+        word.substring(candidateForReplacement1 + 1)
+      word =
+        word.substring(0, candidateForReplacement2) +
+        temp +
+        word.substring(candidateForReplacement2 + 1)
+      break
+    }
+    if (i === 10) return null
+    else if (allWords.includes(word)) return null
+    else {
+      setOriginalWord(ORIGINAL_WORD)
+      return word
+    }
+  }
+
+  function nextWord() {
+    setErroredWord(null)
+    setOriginalWord(null)
+    // setWords([])
+    fetch('https://random-word-api.herokuapp.com/word?number=10')
+      .then((res) => res.json())
+      .then((data) => {
+        const vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+        const consonants = new Set(
+          'bcdfghjklmnpqrstvwxyz'.split('').filter((c) => !vowels.has(c))
+        )
+        let words = data
+        let erroredWord
+        let wordInListOfWordsIndex
+        do {
+          wordInListOfWordsIndex = Math.floor(Math.random() * words.length)
+          let word = words[wordInListOfWordsIndex]
+          erroredWord = createErroredWord(word, consonants, vowels)
+        } while (erroredWord === null)
+        words[wordInListOfWordsIndex] = erroredWord
+        setErroredWord(erroredWord)
+        setDisabled(false)
+        setWords(words)
+      })
+  }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(nextWord, [])
+
+  useEffect(() => {
+    setInitialTime(new Date())
+    setInterval(() => {
+      let temp = Math.round((new Date() - initialTime) / 100) / 10
+      setTime(Number.isInteger(temp) ? `${temp}.0` : temp)
+    }, 100)
+    fetch('https://random-word-api.herokuapp.com/all')
+      .then((res) => res.json())
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      .then((data) => (allWords = data))
+  }, [])
+
+  window.onselectstart = (e) => e.preventDefault()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <link rel="manifest" href="/manifest.json" />
+      <h2 className="Title">Time: {time}s, Round: 10</h2>
+      <div className={`Words ${erroredWord === null ? 'Shrink' : ''}`}>
+        {words.map((word) => (
+          <Word
+            key={word}
+            word={word}
+            erroredWord={erroredWord === word}
+            originalWord={originalWord}
+            nextWord={nextWord}
+            disabled={disabled}
+            setDisabled={setDisabled}
+          />
+        ))}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Word.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+function Word({
+  word,
+  originalWord,
+  erroredWord,
+  nextWord,
+  disabled,
+  setDisabled,
+}) {
+  const [reveal, setReveal] = useState(false)
+
+  function pickWord(e) {
+    if (!disabled) {
+      e.preventDefault()
+      setReveal(true)
+      setDisabled(true)
+      setTimeout(() => {
+        setReveal(false)
+        erroredWord ? nextWord() : setDisabled(false)
+      }, 1000)
+    }
+  }
+  return (
+    <button
+      onClick={pickWord}
+      className="Word"
+      style={{
+        backgroundColor: reveal ? (erroredWord ? 'green' : '#f00') : 'gray',
+      }}
+      disabled={disabled}
+    >
+      {word}
+      {reveal && erroredWord && (
+        <>
+          {' - '}
+          {originalWord}
+        </>
+      )}
+    </button>
+  )
+}
+
+export default Word
